e09e25fbd4367da6d58d6d948de1354fefe1355f
sairajzero <sairaj0630@gmail.com>
2022-05-14 19:31:16 +0530

    Updating price up/down percentage

--- src/_constants.js
+++ src/_constants.js
@@ -16,10 +16,10 @@ module.exports = {
     },
     price: {
         MIN_TIME: 1 * 60 * 60 * 1000, // 1 hr
-        DOWN_RATE: 0.2 / 100, //0.2% dec
-        UP_RATE: 0.5 / 100, //0.5% inc
-        MAX_DOWN_PER_DAY: 4.8 / 100, //max 4.8% dec
-        MAX_UP_PER_DAY: 12 / 100, //max 12% inc
+        DOWN_RATE: 0.05 / 100, //0.05% dec
+        UP_RATE: 0.2 / 100, //0.2% inc
+        MAX_DOWN_PER_DAY: 1 / 100, //max 1% dec
+        MAX_UP_PER_DAY: 4 / 100, //max 4% inc
         CHECK_RATED_SELLER: false,
         TOP_RANGE: 10 / 100, //top 10%
         REC_HISTORY_INTERVAL: 1 * 60 * 60 * 1000, //1 hr
