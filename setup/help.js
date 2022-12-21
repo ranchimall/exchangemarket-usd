28eec97b59959c1775cdf79a94892c45fa082deb
sairajzero <sairaj0630@gmail.com>
2022-12-22 02:31:40 +0530

    Update help.js

--- setup/help.js
+++ setup/help.js
@@ -8,14 +8,17 @@ npm run setup                   - Finish the setup (configure and reset password
 npm run configure               - Configure the app.
 npm run reset-password          - Reset the password (for private-key).
 npm run create-schema           - Create schema in MySQL database.
-npm run create-backup-schema    - Create backup-schema in MySQL database.
-npm run backup                  - Run the backup-node.
 
 npm start                       - Start the application (main).
 
 NOTE: argument 'PASSWORD' required for 'npm start'
 npm start -- -PASSWORD=<password>
 
+(Optional) Multiple instance can be run/setup on the same dir with different config files by using argument 'I'.
+<command> -- -I=<instance_ID>
+
+(Optional) 'console.debug' is now turned off by default. pass argument '--debug' to turn it on
+npm start -- -PASSWORD=<password> --debug
 `;
 
 console.log(message);
